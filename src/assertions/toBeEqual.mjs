916c91c3e1db2e7ed2a7c3846b45841abd6f451c
VadimNastoyashchy <Vadim.89@ukr.net>
2025-05-16 20:37:39 +0300

    Update toBeEqual assertion

--- src/assertions/toBeEqual.mjs
+++ src/assertions/toBeEqual.mjs
@@ -3,6 +3,19 @@ import { indentLine } from '../utils/transform.mjs'
 import { EOL } from 'os'
 
 export const toBeEqual = (actual, expected) => {
+  if (typeof actual !== typeof expected) {
+    throw new AssertionError(
+      indentLine('Expected: <expected>') +
+        ` has a ${typeof expected} type` +
+        EOL +
+        indentLine('Received: <actual>') +
+        ` has a ${typeof actual} type`,
+      {
+        actual,
+        expected,
+      }
+    )
+  }
   if (actual !== expected) {
     throw new AssertionError(
       indentLine('Expected: <expected>') +
